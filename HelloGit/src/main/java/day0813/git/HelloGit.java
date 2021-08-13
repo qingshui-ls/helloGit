ce29d813fb9fdc7618c789aa445ecd0cd8f24238
liangshuang <1664736954@qq.com>
2021-08-13 22:08:46 +0800

    添加文件

---
+++ HelloGit/src/main/java/day0813/git/HelloGit.java
@@ -0,0 +1,11 @@
+package day0813.git;
+
+/**
+ * @authour shuangliang
+ * @date &{DATE} - 22:03
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
